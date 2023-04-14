af3049395ecfac6e05269c8473c59f7c81d0ee4c
Syabetter <125465887+Larilize@users.noreply.github.com>
2023-04-15 01:32:46 +0900

    Update ExampleUse.php

--- ExampleUse.php
+++ ExampleUse.php
@@ -1,1 +1,10 @@
-<?php echo emoji("Syabetter :verifiedblue:",20); ?>
+<?php
+  require 'Smoji/smoji.php'; //IMPORT SMOJI
+?>
+<p><?php 
+  echo emoji("Syabetter :verifiedblue:",20); // emoji('text goes here',size-of-font(px))
+?></p>
+
+<!--  -->
+
+<p class='username' ><?php echo emoji("Syabetter :verifiedblue:",20); ?></p>
