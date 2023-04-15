4af1e865600aaa59356026f69b640a1e85f9184f
Syabetter OSS <125465887+Syabetter@users.noreply.github.com>
2023-04-15 13:23:50 +0900

    update : fix and Add emoji
    I added two emoji :smile: and :yum:
    Fixed Smoji ( "vertical-align: middle;"  to "vertical-align: top;" )

--- SMoji/smoji.php
+++ SMoji/smoji.php
@@ -2,17 +2,21 @@
 
 function replaceEmojisWithImages($text, $emojiSize)
 {
-    $emojiPattern = '/:(\w+):/'; 
-    $emojiPath = "assets/"; 
+    $emojiPattern = '/:(\w+):/'; // 絵文字名の正規表現パターン
+    $emojiPath = "Smoji/assets/"; // 絵文字ファイルのパス
+
+    // 絵文字名を正規表現で検索し、対応するpngファイル名に置き換える
     $text = preg_replace_callback($emojiPattern, function($matches) use ($emojiPath, $emojiSize) {
-        $emojiName = $matches[1]; 
-        $emojiFile = $emojiPath . $emojiName . ".png"; 
-        if (file_exists($emojiFile)) { 
-            return '<img src="' . $emojiFile . '" alt=":' . $emojiName . ':" style="height:' . $emojiSize . ';width:auto;display:inline-block;vertical-align:middle;">';
+        $emojiName = $matches[1]; // 絵文字名を取得
+        $emojiFile = $emojiPath . $emojiName . ".png"; // 絵文字ファイルのパスを生成
+        if (file_exists($emojiFile)) { // 絵文字ファイルが存在する場合
+            return '<img src="' . $emojiFile . '" alt=":' . $emojiName . ':" style="height:' . $emojiSize . ';width:auto;display:inline-block;vertical-align:top;">';
+            // 絵文字画像のHTMLコードに置き換え（高さを指定し、幅は自動調整）
         } else {
-            return ':' . $emojiName . ':'; 
+            return ':' . $emojiName . ':'; // 絵文字ファイルが存在しない場合は変更しない
         }
     }, $text);
+
     return $text;
 }
 
